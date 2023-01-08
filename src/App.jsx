5dd957fa62a12e2c9b4a08e118a36a25361e5755
Kanch Ruansiripiyakul <digitazine@gmail.com>
2023-01-08 23:08:46 +0700

    ADD Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Hello</h1>
+      <NavBar/>
     </div>
   );
 }
